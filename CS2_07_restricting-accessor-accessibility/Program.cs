d4444c70d860927fd1993dc1abab5796c8a0b45a
m2wasabi <m2wasabi@gmail.com>
2017-07-30 16:15:37 +0900

    Add Project C#2.0 point_07 restricting-accessor-accessibility

---
+++ CS2_07_restricting-accessor-accessibility/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CS2_07_restricting_accessor_accessibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+            // a.PropertyA = 1; <- これはできない
+            a.SetPropertyA(10);
+            int propertyValue = a.PropertyA;
+            Console.WriteLine(propertyValue);   // 10
+
+            Console.ReadKey();
+        }
+    }
+
+    class A
+    {
+        public int PropertyA { get; private set; }
+        public void SetPropertyA(int value) { PropertyA = value; }
+    }
+}
