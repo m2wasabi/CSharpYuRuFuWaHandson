4db7a68da1ab519e65b62443362f2960649fc8db
m2wasabi <m2wasabi@gmail.com>
2017-07-30 16:08:52 +0900

    Add Project C#2.0 point_05 nullable-types

---
+++ CS2_05_nullable-types/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CS2_05_nullable_types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? a = 1;
+            a = null;
+            Console.WriteLine(a == null);   // True
+            a = 1;
+            Console.WriteLine(a.Value);     // 1
+
+            Console.ReadKey();
+        }
+    }
+}
