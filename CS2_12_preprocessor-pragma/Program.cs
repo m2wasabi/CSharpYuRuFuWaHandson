2b0ffc9aa73cf2dbee4203c493746885891d871b
m2wasabi <m2wasabi@gmail.com>
2017-07-30 16:49:23 +0900

    Add Project C#2.0 point_12 #pragma プリプロセッサディレクティブ

---
+++ CS2_12_preprocessor-pragma/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CS2_12_preprocessor_pragma
+{
+    class Program
+    {
+        /// <summary>#pragma プリプロセッサディレクティブ</summary>
+        /// <remarks>特殊な命令をコンパイラに指示できる。
+        ///  https://docs.microsoft.com/ja-jp/dotnet/csharp/language-reference/preprocessor-directives/preprocessor-pragma
+        /// </remarks>
+
+        static void Main(string[] args)
+        {
+#pragma warning disable CS0168
+            int i;  // CS0168 の警告（変数未使用）がでない
+#pragma warning restore CS0168
+            int j;  // CS0168 の警告（変数未使用）がでる
+
+            Console.ReadKey();
+        }
+    }
+}
