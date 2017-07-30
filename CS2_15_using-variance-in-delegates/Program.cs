8d59042db7d8765a42831642d414a96ad192d428
m2wasabi <m2wasabi@gmail.com>
2017-07-30 17:17:04 +0900

    Add Project C#2.0 point_15 デリゲートの分散 (共変性と反変性)

---
+++ CS2_15_using-variance-in-delegates/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CS2_15_using_variance_in_delegates
+{
+    /// <summary>デリゲートの分散 (共変性と反変性)</summary>
+    /// <remarks>共変性により、メソッドの戻り値の型をデリゲートに定義されている型のサブクラスにできる。 反変性によりメソッドのパラメーター型をデリゲートのパラメーターの型のスーパークラスにできる。
+    ///  https://docs.microsoft.com/ja-jp/dotnet/csharp/programming-guide/concepts/covariance-contravariance/using-variance-in-delegates
+    /// </remarks>
+
+    class Program
+    {
+        class A { }
+        class A2 : A { }
+        class A3 : A { }
+
+        delegate A2 MyDelegate(A2 a2);
+
+        static void Main(string[] args)
+        {
+            MyDelegate deleg1 = Method1;    // <- これはできる
+            A a = deleg1(new A2());
+            //MyDelegate deleg2 = Method2;  // <- これはできない
+            //MyDelegate deleg3 = Method3;  // <- これはできない
+            Console.ReadKey();
+        }
+
+        static A2 Method1(A a)
+        {
+            return null;
+        }
+
+        static A2 Method2(A3 a)
+        {
+            return null;
+        }
+
+        static A Method3(A2 a)
+        {
+            return null;
+        }
+    }
+}
