83d2bf2bbc4f874bf1a77d85762ed8e4ee3c3ce4
m2wasabi <m2wasabi@gmail.com>
2017-07-30 15:21:34 +0900

    Add Project C#2.0 point_01 Generics

---
+++ CS2_01_generic/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CS2_01_generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> ints = new List<int>();
+            // ints.Add("text"); <- これはできない
+            ints.Add(1);
+            int i = ints[0];
+            Console.WriteLine(i);
+
+            List<string> strings = new List<string>();
+            // strings.Add(1); <- これはできない
+            strings.Add("text");
+            string s = strings[0];
+            Console.WriteLine(s);
+
+            Console.ReadKey();
+        }
+    }
+}
