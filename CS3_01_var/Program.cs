b7452cbf2422e927f1f03c3fed254f31194f9bfc
m2wasabi <m2wasabi@gmail.com>
2017-07-30 17:21:44 +0900

    Add Project C#3.0 point_01 型推論 (var)

---
+++ CS3_01_var/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CS3_01_var
+{
+    /// <summary>型推論 (var)</summary>
+    /// <remarks>変数の宣言時に暗黙の型指定を行える。コンパイラが型を推論してくれる。
+    ///  https://docs.microsoft.com/ja-jp/dotnet/csharp/language-reference/keywords/var
+    /// </remarks>
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var i = 10; // i は int 型の変数
+            i = 20;     // <- これはできる
+                        // i = "text"; // これはできない
+
+            Console.WriteLine(i.GetType().Name);
+
+            Console.ReadKey();
+        }
+    }
+}
