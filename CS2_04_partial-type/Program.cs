f739236d874a6350a7d327a610ffea365568b215
m2wasabi <m2wasabi@gmail.com>
2017-07-30 16:06:13 +0900

    Add Project C#2.0 point_04 partial-type

---
+++ CS2_04_partial-type/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CS2_04_partial_type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+            a.A1(); // A1
+            a.A2(); // A2
+
+            Console.ReadKey();
+        }
+    }
+
+    partial class A
+    {
+        public void A1() { Console.WriteLine("A1"); }
+    }
+
+    partial class A
+    {
+        public void A2() { Console.WriteLine("A2"); }
+    }
+}
