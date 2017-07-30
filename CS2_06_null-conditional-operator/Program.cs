cb5114e58fba10bd239fa1dcba47a808aaab8355
m2wasabi <m2wasabi@gmail.com>
2017-07-30 16:12:11 +0900

    Add Project C#2.0 point_06 null-conditional-operator

---
+++ CS2_06_null-conditional-operator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CS2_06_null_conditional_operator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "a";
+            string b = null;
+            Console.WriteLine(a);   // a
+            Console.WriteLine(b ?? "b is null");    // b is null
+
+            Console.ReadKey();
+        }
+    }
+}
