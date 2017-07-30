cd37c1f81aaa50431986f680c342ee1bc0d263db
m2wasabi <m2wasabi@gmail.com>
2017-07-30 17:23:47 +0900

    Add Project C#3.0 point_02 配列の型推論 (暗黙的に型指定される配列)

---
+++ CS3_02_implicitly-typed-arrays/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CS3_02_implicitly_typed_arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ints = new[] { 0, 20, 30 }; // ints は int 型の配列
+            ints[0] = 10;     // <- これはできる
+                              // ints[0] = "text"; <- これはできない
+            foreach (var i in ints)
+            {
+                Console.WriteLine(i); // 10
+                                      // 20
+                                      // 30
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
