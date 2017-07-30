c1d6a045845e9dc73f8cfa70074e1360b975a6a8
m2wasabi <m2wasabi@gmail.com>
2017-07-30 16:01:09 +0900

    Add Project C#2.0 point_03 yield

---
+++ CS2_03_yield/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CS2_03_yield
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (int number in Values())
+            {
+                Console.WriteLine(number.ToString());    // 1
+                                                         // 2
+                                                         // 3
+            }
+
+            Console.ReadKey();
+        }
+
+        public static IEnumerable<int> Values()
+        {
+            yield return 1;
+            yield return 2;
+            yield return 3;
+        }
+    }
+}
