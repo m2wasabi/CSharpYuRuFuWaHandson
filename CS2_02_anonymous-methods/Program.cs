61e825f3c5a1006c6ed52f0a17a5743a03a48f07
m2wasabi <m2wasabi@gmail.com>
2017-07-30 15:35:36 +0900

    Add Project C#2.0 point_02 Anonymous Methods

---
+++ CS2_02_anonymous-methods/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CS2_02_anonymous_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //　[推奨されません(※)] delegate をインライン コードで生成できる。 (※)C# 3.0 で追加されたラムダ式を使用してください。
+            System.Threading.Timer t = new System.Threading.Timer(
+                delegate (object state) { Console.WriteLine("delegate"); } // 匿名メソッド
+                , null, 0, 1000
+                );
+            // "delegate" を繰り返し出力
+
+            Console.ReadKey();
+        }
+    }
+}
