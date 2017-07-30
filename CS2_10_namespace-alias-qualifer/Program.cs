132fccda934eb8c24bf909c45474d7c94b0eb0e8
m2wasabi <m2wasabi@gmail.com>
2017-07-30 16:45:04 +0900

    Add Project C#2.0 point_10 :: 演算子

---
+++ CS2_10_namespace-alias-qualifer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CS2_10_namespace_alias_qualifer
+{
+    class Program
+    {
+        /// <summary>:: 演算子</summary>
+        /// <remarks>名前空間エイリアス修飾子として global を指定すると、常にグローバル名前空間で検索が実行される。
+        ///  https://docs.microsoft.com/ja-jp/dotnet/csharp/language-reference/operators/namespace-alias-qualifer
+        /// </remarks>
+
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("csharp");         // MyMethod
+            global::System.Console.WriteLine("csharp"); // csharp
+
+            global::System.Console.ReadKey();
+        }
+    }
+
+    class System
+    {
+        internal class Console
+        {
+            internal static void WriteLine(string text)
+            {
+                global::System.Console.WriteLine("MyMethod");
+            }
+        }
+    }
+}
