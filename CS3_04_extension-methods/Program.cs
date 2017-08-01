2ea722492743bee4101571bcc39aeadf8843e5cf
m2wasabi <m2wasabi@gmail.com>
2017-08-01 18:49:31 +0900

    Add Project C#3.0 point_04 拡張メソッド

---
+++ CS3_04_extension-methods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CS3_04_extension_methods
+{
+    /// <summary>拡張メソッド</summary>
+    /// <remarks>既存の型に外からメソッドを追加できる。
+    ///  既存の型に外からメソッドを追加できる。
+    /// </remarks>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "text";
+            Console.WriteLine(s.HasValue());    // True
+            s = null;
+            Console.WriteLine(s.HasValue());    // False
+
+            Console.ReadKey();
+        }
+    }
+
+    static class StringExtensions
+    {
+        internal static bool HasValue(this string value)
+        {
+            return !string.IsNullOrEmpty(value);
+        }
+    }
+}
