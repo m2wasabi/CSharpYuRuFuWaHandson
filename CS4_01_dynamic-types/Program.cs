4373df3792a6cf1c0c3165f88e65873e999be485
m2wasabi <m2wasabi@gmail.com>
2017-08-01 19:24:19 +0900

    Add Project C#4.0 point_01 dynamic 型

---
+++ CS4_01_dynamic-types/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CS4_01_dynamic_types
+{
+    /// <summary>dynamic 型</summary>
+    /// <remarks>dynamic 型はコンパイル時の型チェックを行わず、実行時に解決される。
+    ///  https://docs.microsoft.com/ja-jp/dotnet/csharp/language-reference/keywords/dynamic
+    /// </remarks>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                dynamic a = 1;
+                Console.WriteLine(a);                   // 1
+                Console.WriteLine(a.GetType().Name);    // Int32
+
+                a = a + 1;
+                Console.WriteLine(a);                   // 2
+
+                a = "text";
+                a = a + 1;
+                Console.WriteLine(a);                   // text1
+                Console.WriteLine(a.GetType().Name);    // String
+                Console.WriteLine(a.ToString());        // text1
+
+                Console.WriteLine(a.ToString2());       // <- 実行時エラー「'string' に 'ToString2' の定義がありません」
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
